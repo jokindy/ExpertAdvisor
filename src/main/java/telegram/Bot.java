7db15d7f04ff541eb316bf7d5aa04c3df6dd5b9b
jokindy <95441202+jokindy@users.noreply.github.com>
2022-09-07 21:04:43 +0300

    add a proc

--- src/main/java/telegram/Bot.java
+++ src/main/java/telegram/Bot.java
@@ -16,6 +16,7 @@ import util.KeyboardMaker;
 import java.io.File;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class Bot extends TelegramLongPollingBot {
@@ -50,33 +51,41 @@ public final class Bot extends TelegramLongPollingBot {
         long chatId = callbackQuery.getMessage().getChatId();
         Random random = new Random();
         switch (taste) {
-            case "Сладенькое" -> {
+            case "Сладенькое":
                 int size = Mix.sweetMixes.size();
                 String advice = Mix.sweetMixes.get(random.nextInt(size));
                 fillWaiting(chatId, Greetings.sweetGreetings);
                 execute(getMessage(chatId, advice));
-            }
-            case "Кисленькое" -> {
-                int size = Mix.sourMixes.size();
-                String advice = Mix.sourMixes.get(random.nextInt(size));
+                break;
+            case "Кисленькое":
+                size = Mix.sourMixes.size();
+                advice = Mix.sourMixes.get(random.nextInt(size));
                 fillWaiting(chatId, Greetings.sourGreetings);
                 execute(getMessage(chatId, advice));
-            }
-            case "Странное" -> {
-                int size = Mix.strangeMixes.size();
-                String advice = Mix.strangeMixes.get(random.nextInt(size));
+                break;
+            case "Странное":
+                size = Mix.strangeMixes.size();
+                advice = Mix.strangeMixes.get(random.nextInt(size));
                 fillWaiting(chatId, Greetings.strangeGreetings);
                 execute(getMessage(chatId, advice));
-            }
-            case "Двойное яблоко" -> {
+                break;
+            case "Двойное яблоко":
                 execute(getMessage(chatId, Greetings.doubleAppleGreetings.get(random.nextInt(3))));
                 TimeUnit.SECONDS.sleep(1);
                 execute(getMessage(chatId, "Больной ублюдок..."));
-            }
-            case "Вконтакте" -> execute(getMessage(chatId, "vk.com/expertbar"));
-            case "Телеграм" -> execute(getMessage(chatId, "@ExpertBar"));
-            case "Инстаграм" -> execute(getMessage(chatId, "instagram.com/expertbar"));
-            case "Телефон" -> execute(getMessage(chatId, "+79219135164"));
+                break;
+            case "Вконтакте":
+                execute(getMessage(chatId, "vk.com/expertbar"));
+                break;
+            case "Телеграм":
+                execute(getMessage(chatId, "@ExpertBar"));
+                break;
+            case "Инстаграм":
+                execute(getMessage(chatId, "instagram.com/expertbar"));
+                break;
+            case "Телефон":
+                execute(getMessage(chatId, "+79219135164"));
+                break;
         }
         TimeUnit.SECONDS.sleep(1);
         execute(getMessage(chatId, "Спасибо за использование! Выберете команду:"));
@@ -87,47 +96,52 @@ public final class Bot extends TelegramLongPollingBot {
         if (message.hasText()) {
             String command = message.getText();
             switch (command) {
-                case "/start" -> execute(SendMessage.builder()
-                        .text("Привет! Я помогу тебе выбрать вкусный кальян в Эксперте! Используй клавиатуру, " +
-                                "чтобы начать работу")
-                        .replyMarkup(KeyboardMaker.getMainMenuKeyboard())
-                        .chatId(message.getChatId().toString())
-                        .build());
-                case "Получить совет" -> {
+                case "/start":
+                    execute(SendMessage.builder()
+                            .text("Привет! Я помогу тебе выбрать вкусный кальян в Эксперте! Используй клавиатуру, " +
+                                    "чтобы начать работу")
+                            .replyMarkup(KeyboardMaker.getMainMenuKeyboard())
+                            .chatId(message.getChatId().toString())
+                            .build());
+                    break;
+                case "Получить совет":
                     List<String> tasteNames = Stream.of(Taste.values())
                             .map(Taste::getDescription)
-                            .toList();
+                            .collect(Collectors.toList());
                     List<List<InlineKeyboardButton>> buttons = KeyboardMaker.getButtons(tasteNames);
                     execute(SendMessage.builder()
                             .text("Какие предпочтения по вкусу?")
                             .chatId(message.getChatId().toString())
                             .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttons).build())
                             .build());
-                }
-                case "Скачать меню" -> {
+                    break;
+                case "Скачать меню":
                     SendDocument sendDocument = getDocument(message.getChatId());
                     execute(sendDocument);
-                }
-                case "Связаться" -> {
+                    break;
+                case "Связаться":
                     List<String> contactNames = Stream.of(Contact.values())
                             .map(Contact::getDescription)
-                            .toList();
-                    List<List<InlineKeyboardButton>> buttons = KeyboardMaker.getButtons(contactNames);
+                            .collect(Collectors.toList());
+                    buttons = KeyboardMaker.getButtons(contactNames);
                     execute(SendMessage.builder()
                             .text("Где вам удобнее связаться?")
                             .chatId(message.getChatId().toString())
                             .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttons).build())
                             .build());
-                }
-                case "Помощь" -> execute(SendMessage.builder()
-                        .text("Привет! Это бот, который поможет тебе выбрать вкусный кальян в Эксперт Баре! " +
-                                "Используй клавиатуру, чтобы начать работу")
-                        .chatId(message.getChatId().toString())
-                        .build());
-                default -> execute(SendMessage.builder()
-                        .text("Такая команда не поддерживается")
-                        .chatId(message.getChatId().toString())
-                        .build());
+                    break;
+                case "Помощь":
+                    execute(SendMessage.builder()
+                            .text("Привет! Это бот, который поможет тебе выбрать вкусный кальян в Эксперт Баре! " +
+                                    "Используй клавиатуру, чтобы начать работу")
+                            .chatId(message.getChatId().toString())
+                            .build());
+                    break;
+                default:
+                    execute(SendMessage.builder()
+                            .text("Такая команда не поддерживается")
+                            .chatId(message.getChatId().toString())
+                            .build());
             }
         }
     }
